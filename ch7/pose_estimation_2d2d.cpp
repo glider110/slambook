b1315e23bc9f4017839304e2c261894c3e358be0
郭小凡 <glider.guo@ankobot.com>
2022-01-20 17:15:03 +0800

    对极约束计算位姿;

--- ch7/pose_estimation_2d2d.cpp
+++ ch7/pose_estimation_2d2d.cpp
@@ -3,6 +3,12 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <list>
+#include <chrono>
 // #include "extra.h" // use this if in OpenCV2 
 using namespace std;
 using namespace cv;
@@ -40,11 +46,76 @@ int main ( int argc, char** argv )
     vector<KeyPoint> keypoints_1, keypoints_2;
     vector<DMatch> matches;
     find_feature_matches ( img_1, img_2, keypoints_1, keypoints_2, matches );
-    cout<<"一共找到了"<<matches.size() <<"组匹配点"<<endl;
+    //光流计算关键点
+    #if 1
 
-    //-- 估计两张图像间运动
+        int maxCount = 50;//最大特征点数
+        double minDis = 20;//：对于初选出的角点而言，如果
+        //在其周围minDis范围内存在其他更强角点，则将此角点删除
+        double qLevel = 0.01;//角点的品
+ 
+        // 对其他帧用LK跟踪特征点
+        vector<cv::Point2f> next_keypoints; 
+        vector<cv::Point2f> prev_keypoints;
+        vector<unsigned char> status;
+        vector<float> error; 
+        Mat img_1_gray;
+        cout<<"prev_keypoints: "<<prev_keypoints.size()<<endl;
+        cvtColor(img_1, img_1_gray, CV_RGB2GRAY);
+        goodFeaturesToTrack(img_1_gray, prev_keypoints, maxCount, qLevel, minDis);//角点识别
+        chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+        cv::calcOpticalFlowPyrLK( img_1, img_2, prev_keypoints, next_keypoints, status, error );
+        chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+        chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>( t2-t1 );
+        cout<<"LK Flow use time："<<time_used.count()<<" seconds."<<endl;
+        cout<<"prev_keypoints: "<<prev_keypoints.size()<<endl;
+        cout<<"next_keypoints: "<<next_keypoints.size()<<endl;
+        cout<<"status: "<<status.size()<<endl;
+            //-- 计算基础矩阵
+        Mat fundamental_matrix;
+        fundamental_matrix = findFundamentalMat ( prev_keypoints, next_keypoints, CV_FM_8POINT );
+        cout<<"fundamental_matrix is "<<endl<< fundamental_matrix<<endl;
+
+        //-- 计算本质矩阵
+        Point2d principal_point ( 325.1, 249.7 );	//相机光心, TUM dataset标定值
+        double focal_length = 521;			//相机焦距, TUM dataset标定值
+        Mat essential_matrix;
+        essential_matrix = findEssentialMat ( prev_keypoints, next_keypoints, focal_length, principal_point );
+        cout<<"essential_matrix is "<<endl<< essential_matrix<<endl;
+
+        //-- 计算单应矩阵
+        Mat homography_matrix;
+        homography_matrix = findHomography ( prev_keypoints, next_keypoints, RANSAC, 3 );
+        cout<<"homography_matrix is "<<endl<<homography_matrix<<endl;
+
+        //-- 从本质矩阵中恢复旋转和平移信息.
+        Mat R_optical,t_optical;
+        recoverPose ( essential_matrix, prev_keypoints, next_keypoints, R_optical, t_optical, focal_length, principal_point );
+        cout<<"R is "<<endl<<R_optical<<endl;
+        cout<<"t is "<<endl<<t_optical<<endl;
+        Mat img_2_copy2 = img_2.clone();
+        for (int i = 0; i < next_keypoints.size(); i++)
+		{
+			circle(img_2_copy2, next_keypoints[i], 3, Scalar(0, 255, 0), 2);
+			line(img_2_copy2, prev_keypoints[i], next_keypoints[i], Scalar(255, 0, 0), 2);//画出两帧之间的光流变化
+		}
+		
+        namedWindow("OPTICAL",CV_WINDOW_AUTOSIZE);
+        imshow("OPTICAL", img_2_copy2);
+        waitKey(100000);
+     #endif
+
+
+    
+
+    // -- 估计两张图像间运动
     Mat R,t;
+    Mat R_vec;
     pose_estimation_2d2d ( keypoints_1, keypoints_2, matches, R, t );
+    Rodrigues(R, R_vec);
+    cout << "Rodrigues = " << R_vec << endl;
+    cout << "roll degree = " << R_vec*180/ CV_PI << endl;
+
 
     //-- 验证E=t^R*scale
     Mat t_x = ( Mat_<double> ( 3,3 ) <<
@@ -117,6 +188,29 @@ void find_feature_matches ( const Mat& img_1, const Mat& img_2,
             matches.push_back ( match[i] );
         }
     }
+
+
+    //-- 把匹配点转换为vector<Point2f>的形式
+    vector<Point2f> points1;
+    vector<Point2f> points2;
+
+    for ( int i = 0; i < ( int ) matches.size(); i++ )
+    {
+        points1.push_back ( keypoints_1[matches[i].queryIdx].pt );
+        points2.push_back ( keypoints_2[matches[i].trainIdx].pt );
+    }
+    cout<<"一共找到了XXX"<<points1.size() <<"组匹配点"<<endl;
+    cout<<"一共找到了XXX"<<points2.size() <<"组匹配点"<<endl;
+    Mat img_2_copy1 = img_2.clone();
+    for (int i = 0; i < points2.size(); i++)
+    {
+        circle(img_2_copy1, points2[i], 3, Scalar(0, 255, 0), 2);
+        line(img_2_copy1, points1[i], points2[i], Scalar(0, 0, 255), 2);//画出两帧之间的光流变化
+    }
+    
+    namedWindow("ORB",CV_WINDOW_AUTOSIZE);
+    imshow("ORB", img_2_copy1);
+    // waitKey(0);
 }
 
 
@@ -155,7 +249,7 @@ void pose_estimation_2d2d ( std::vector<KeyPoint> keypoints_1,
 
     //-- 计算本质矩阵
     Point2d principal_point ( 325.1, 249.7 );	//相机光心, TUM dataset标定值
-    double focal_length = 521;			//相机焦距, TUM dataset标定值
+    double focal_length = 300;			//相机焦距, TUM dataset标定值
     Mat essential_matrix;
     essential_matrix = findEssentialMat ( points1, points2, focal_length, principal_point );
     cout<<"essential_matrix is "<<endl<< essential_matrix<<endl;
